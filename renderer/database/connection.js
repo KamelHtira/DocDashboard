5b4aa0ba7da3705e58d28115bf90b3e680a565b9
KamelHtira <Kamel.htira@isamm.u-manouba.tn>
2023-02-03 18:48:35 +0100

    create branchs, test server

--- renderer/database/connection.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose";
-mongoose.set('strictQuery', true);
-const connectMongo = async() => mongoose.connect("mongodb+srv://kamel:kamel@cluster0.wejj0ir.mongodb.net/?retryWrites=true&w=majority")
-export default connectMongo;
